5bd75da8c9c3be931a28d13ac0b81d4da13ef506
SachaAttiach <settiach@hotmail.co.uk>
2022-04-11 12:53:07 +0300

    app structure

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const apiKey = process.env.API_KEY
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+
+    return (
+        <div className="app__wrapper">
+            <Chat>
+                <ChannelListContainer 
+
+                />
+                <ChannelContainer 
+
+                />
+            </Chat>
+        </div>
+    );
+}
+
+export default App
