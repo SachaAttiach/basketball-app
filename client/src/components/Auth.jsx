f338769d8745b00475550a01036cee21b8d22c33
SachaAttiach <settiach@hotmail.co.uk>
2022-04-11 18:13:19 +0300

    edit channel and fixed name appearance

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -30,7 +30,7 @@ const Auth = () => {
         const { username, password, phoneNumber, avatarURL } = form;
 
         const URL = 'http://localhost:5000/auth';
-        
+        //form.fullname fix because id was coming up.
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
         });
